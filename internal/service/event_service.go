2365890611fdeec3ced4670865cfadca0bfee8ae
bryan lin <920616bryanlin@gmail.com>
2025-08-14 14:17:19 +0800

    chore: remove todo comment, error handling is done by grpc service layer

--- internal/service/event_service.go
+++ internal/service/event_service.go
@@ -163,7 +163,7 @@ func (s *EventService) PatchEvent(ctx context.Context, brandID string, req *Patc
 	// Get existing event
 	existingEvent, err := s.GetEvent(ctx, brandID, req.ID)
 	if err != nil {
-		return nil, err // todo: handle not found
+		return nil, err
 	}
 
 	if err := s.validateEventChanges(existingEvent, req); err != nil {
